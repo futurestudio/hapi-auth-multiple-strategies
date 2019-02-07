ef3d49ff724593f681c0978458fe5e00dab0116e
Marcus Pöhls <marcus.poehls@gmail.com>
2019-02-07 10:30:20 +0100

    ensure scope array

--- test/index.js
+++ test/index.js
@@ -13,7 +13,7 @@ async function prepareServer () {
   const server = new Hapi.Server()
   await server.register(Plugin)
 
-  server.auth.scheme('succeeding', function (_, options) {
+  server.auth.scheme('succeeding', (_, options) => {
     return {
       authenticate (_, h) {
         return h.authenticated({ credentials: options.value, artifacts: options.artifacts })
@@ -21,7 +21,16 @@ async function prepareServer () {
     }
   })
 
-  server.auth.scheme('failing', function () {
+  server.auth.scheme('succeeding-slow', (_, options) => {
+    return {
+      async authenticate (_, h) {
+        await new Promise(resolve => setTimeout(resolve, 500))
+        return h.authenticated({ credentials: options.value })
+      }
+    }
+  })
+
+  server.auth.scheme('failing', () => {
     return {
       authenticate (_, h) {
         return h.unauthenticated(Boom.unauthorized())
@@ -32,6 +41,7 @@ async function prepareServer () {
   server.auth.strategy('firstAuth', 'succeeding', { value: { first: 1 }, artifacts: { name: 'Marcus' } })
   server.auth.strategy('secondAuth', 'succeeding', { value: { second: 2, scope: ['user', 'admin'] } })
   server.auth.strategy('stringAuth', 'succeeding', { value: 'string' })
+  server.auth.strategy('slowAuth', 'succeeding-slow', { value: { slow: true, scope: 'slow' } })
   server.auth.strategy('failAuth', 'failing')
 
   server.auth.strategy('first', 'multiple-strategies', {
@@ -48,6 +58,10 @@ async function prepareServer () {
 
   server.auth.strategy('firstSecondString', 'multiple-strategies', {
     strategies: ['firstAuth', 'secondAuth', 'stringAuth']
+  })
+
+  server.auth.strategy('secondSlow', 'multiple-strategies', {
+    strategies: ['secondAuth', 'slowAuth']
   })
 
   server.auth.strategy('firstFail', 'multiple-strategies', {
@@ -73,6 +87,16 @@ async function prepareServer () {
       method: 'GET',
       path: '/first/string',
       config: { auth: 'firstString', handler }
+    },
+    {
+      method: 'GET',
+      path: '/second/slow',
+      config: { auth: { strategy: 'secondSlow', scope: 'slow' }, handler }
+    },
+    {
+      method: 'GET',
+      path: '/second/slow/scope-fail',
+      config: { auth: { strategy: 'secondSlow', scope: 'a-scope-not-in-credentials' }, handler }
     },
     {
       method: 'GET',
@@ -107,6 +131,35 @@ describe('Multiple Strategies Scheme', () => {
       },
       strategy: 'firstSecond'
     })
+  })
+
+  it('authenticates multiple strategies and handles string scopes', async () => {
+    const server = await prepareServer()
+    const res = await server.inject({
+      method: 'GET',
+      url: '/second/slow'
+    })
+
+    expect(res.statusCode).to.equal(200)
+    expect(res.result).to.include({
+      isAuthenticated: true,
+      credentials: {
+        scope: ['user', 'admin', 'slow'],
+        secondAuth: { second: 2, scope: ['user', 'admin'] },
+        slowAuth: { slow: true, scope: 'slow' }
+      },
+      strategy: 'secondSlow'
+    })
+  })
+
+  it('fails authorization with wrong scopes', async () => {
+    const server = await prepareServer()
+    const res = await server.inject({
+      method: 'GET',
+      url: '/second/slow/scope-fail'
+    })
+
+    expect(res.statusCode).to.equal(403)
   })
 
   it('defaults to empty scope array', async () => {
